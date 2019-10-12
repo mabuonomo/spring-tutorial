1830ef249193db690020957ebff374ead4a16378
Mario Buonomo <ma.buonomo@gmail.com>
2019-10-12 09:47:26 +0200

    init new tutorial

---
+++ java_object_to_excel/src/main/java/it/mabuonomo/javatoexcel/JavatoexcelApplication.java
@@ -0,0 +1,13 @@
+package it.mabuonomo.javatoexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatoexcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatoexcelApplication.class, args);
+	}
+
+}
